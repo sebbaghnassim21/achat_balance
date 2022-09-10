b8f845beafca8a0d8573ebaedb5d7f3863d24c4d
frappe <frappe@ubuntu.vm>
2022-09-10 08:33:04 +0000

    arrivage

---
+++ acaht_balance/achat_balance/doctype/arrivage/arrivage.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, sebbagh nassim and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Arrivage', {
+	// refresh: function(frm) {
+
+	// }
+});
