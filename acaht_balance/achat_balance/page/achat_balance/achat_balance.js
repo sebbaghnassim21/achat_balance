8e75d122c88b8387c7707ee882390c2192cc67d3
frappe <frappe@ubuntu.vm>
2022-09-26 12:04:25 +0000

    emballage final

---
+++ acaht_balance/achat_balance/page/achat_balance/achat_balance.js
@@ -0,0 +1,7 @@
+frappe.pages['achat-balance'].on_page_load = function(wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'achat balance',
+		single_column: true
+	});
+}
